af619bd5873491b0a020950013a8accda96c7d57
Guilherme Raposo <gui.raposo36@gmail.com>
2022-03-22 10:39:03 -0300

    feat: created routes

---
+++ src/routes.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import UpBar from "./components/UpBar";
+import Healthy from "./views/Healthy";
+import Blog from "./views/Blog";
+import Join from "./views/Join";
+import Register from "./views/Register";
+
+export default function myRoutes() {
+  return (
+    <Router>
+      <UpBar />
+      <Routes>
+        <Route path="/" element={<Healthy />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
